ad3c9e10d70fe75b4ba88410e98ef84e033d3210
shrishtisemwal002 <shrishti.semwal@techsuperiors.com>
2024-04-11 14:01:57 +0530

    email and date validation added

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-// window.onload = function() {
-//     clearForm();
-// };
+// Function for clearing the form on navigating back or forward 
 
 window.addEventListener("pageshow", function(event) {
 
@@ -10,8 +8,9 @@ window.addEventListener("pageshow", function(event) {
 
 });
 
+// this function will be invoked whenever the input is changed or the form is submitted 
 function validateForm() {
-    debugger;
+
     var firstName = document.getElementById("firstName").value;
     var lastName = document.getElementById("lastName").value;
     var email = document.getElementById("inputEmail").value;
@@ -19,11 +18,14 @@ function validateForm() {
     var confirmPassword = document.getElementById("confirmPassword").value;
     var phoneNumber = document.getElementById("phoneNumber").value;
     var adhaarNumber = document.getElementById("adhaarNumber").value;
-    var gender = document.querySelector('input[name="gender"]').value;
+    var gender = document.getElementsByName("gender");
+    var dob = document.getElementById("dob").value;
     var address = document.getElementById("address").value;
     var pinCode = document.getElementById("pinCode").value;
 
     var isValid = true;
+
+    // validation for first name
 
     // if (!isString(firstName)) {
     if (!isStringReg(firstName)) {
@@ -35,6 +37,9 @@ function validateForm() {
         document.getElementById("invalidFirstName").innerHTML = "";
     }
 
+    // validation for first name
+
+    // if(isString(lastName)) {
     if (!isStringReg(lastName)) {
         document.getElementById("lastName").style.border = "1px solid red";
         document.getElementById("invalidLastName").innerHTML = "Invalid Last Name";
@@ -44,7 +49,10 @@ function validateForm() {
         document.getElementById("invalidLastName").innerHTML = "";
     } 
 
-    if(!validEmail(email)) {
+    // validation for email
+
+    // if(!validEmail(email)) {
+    if(!validEmailReg(email)) {
         document.getElementById("inputEmail").style.border = "1px solid red";
         document.getElementById("invalidEmail").innerHTML = "Invalid Email";
         isValid = false;
@@ -52,6 +60,8 @@ function validateForm() {
         document.getElementById("inputEmail").style.border = "none";
         document.getElementById("invalidEmail").innerHTML = "";
     } 
+
+    // validation for password
 
     //if(!isValidPassword(password)){
     if(!isValidPasswordReg(password)){
@@ -63,6 +73,8 @@ function validateForm() {
         document.getElementById("invalidPassword").innerHTML = "";
     }
 
+    // validation for confirm password
+
     if(!(password === confirmPassword)){
         document.getElementById("confirmPassword").style.border = "1px solid red";
         document.getElementById("confirmPasswordMatch").innerHTML = "Password does not match";
@@ -71,6 +83,8 @@ function validateForm() {
         document.getElementById("confirmPassword").style.border = "none";
         document.getElementById("confirmPasswordMatch").innerHTML = "";
     }
+
+    // validation for phone number
 
     // if ((isNaN(phoneNumber) || phoneNumber.length !== 10) || (phoneNumber.includes("e"))) {
     if(!validPhoneNumber(phoneNumber)) {
@@ -82,6 +96,8 @@ function validateForm() {
         document.getElementById("invalidPhoneNumber").innerHTML = "";
     }
 
+    // validation for adhaar number
+
     // if ((isNaN(adhaarNumber) || adhaarNumber.length !== 12) || (adhaarNumber.includes("e"))) {
     if(!validAdhaarNumber(adhaarNumber)) {
         document.getElementById("adhaarNumber").style.border = "1px solid red";
@@ -92,6 +108,29 @@ function validateForm() {
         document.getElementById("invalidAdhaarNumber").innerHTML = "";
     }
 
+    // validation for gender
+
+    if(!validGender(gender)) {
+        document.getElementById("invalidGender").innerHTML = "Please select a gender";
+        isValid = false;
+    } else {
+        document.getElementById("invalidGender").innerHTML = "";
+    }
+
+    // validation for date of birth 
+
+    if(!validDOB(dob)) {
+        document.getElementById("dob").style.border = "1px solid red";
+        document.getElementById("invalidDOB").innerHTML = "Invalid Date Of Birth";
+        isValid = false;
+
+    } else {
+        document.getElementById("dob").style.border = "none";
+        document.getElementById("invalidDOB").innerHTML = "";
+    }
+
+    // validation for address
+
     if(address.length == 0) {
         document.getElementById("address").style.border = "1px solid red";
         document.getElementById("invalidAddress").innerHTML = "Please Enter Address";
@@ -100,6 +139,8 @@ function validateForm() {
         document.getElementById("address").style.border = "none";
         document.getElementById("invalidAddress").innerHTML = "";
     }
+
+    // validation for pincode
 
     // if ((isNaN(pinCode) || pinCode.length !== 6) || (pinCode.includes("e"))) {
     if(!validPinCode(pinCode)) {
@@ -114,10 +155,13 @@ function validateForm() {
     return isValid;
 }
 
+
+//for resetting the form 
 function clearForm() {
-    // Reset the form
     document.getElementById("registrationForm").reset();
 }
+
+// function for checking whether the input field is string only  
 
 function isString(str) {
     var flag = true;
@@ -130,6 +174,8 @@ function isString(str) {
     }
     return flag;
 }
+
+// function to check the validity of the email input field 
 
 function validEmail(email) {
     let atIndex = email.indexOf("@");
@@ -158,6 +204,8 @@ function validEmail(email) {
     return true;
 }
 
+// function for checking the validity of password 
+
 function isValidPassword(password) {
     let num=false;
     let specialChar=false;
@@ -181,37 +229,89 @@ function isValidPassword(password) {
     return num && char && specialChar; 
 }
 
-//regex
+
+//  function for checking the validity of date of birth
+
+function validDOB(dob) {
+    var currentDate = new Date();
+    var inputDOB = new Date(dob);
+
+    if(inputDOB > currentDate) {
+        return false;
+    }
+    return true;
+}
+
+// function for checking gender radio button
+
+function validGender(gender) {
+
+    let isChecked = false;
+    for (let i = 0; i < gender.length; i++) {
+        if (gender[i].checked) {
+            isChecked = true;
+            break;
+        }
+    }
+    return isChecked;
+
+}
+
+//regex functions
+
+// for string input field
+
 function isStringReg(str) {
     var regex = /^[a-zA-Z]+$/;
     return regex.test(str);
 
 }
 
+// for email 
+
 function validEmailReg(email) {
     var regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     return regex.test(email);
 }
 
+// for password validation 
 function isValidPasswordReg(password) {
     var regex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
     return regex.test(password);
 }
+
+// for phone number validtion
 
 function validPhoneNumber(phoneNumber) {
     var regex = /^\d{10}$/;
     return regex.test(phoneNumber);
 }
 
+// for adhaar number validation 
+
 function validAdhaarNumber(adhaarNumber) {
     var regex = /^\d{12}$/;
     return regex.test(adhaarNumber);
 }
+
+// for pincode validation 
 
 function validPinCode(pinCode) {
     var regex = /^\d{6}$/;
     return regex.test(pinCode);
 }
 
+// for showing or hiding the password on clicking the eye icon
+
+function showPassword(input) {
+    var inputField = document.getElementById(input);
+
+    if(inputField.type == "password") {
+        inputField.type = "text";
+    }
+    else {
+        inputField.type="password";
+    }
+}
 
 
